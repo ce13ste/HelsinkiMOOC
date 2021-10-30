4295ec472b40eda9f331e2d3f58bef70a6380407
ce13ste <celeste.lhr@live.com>
2021-10-30 23:50:13 +0800

    Part 1c

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+import React, { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-   ]
-  }
+  const [ counter, setCounter ] = useState(0)
+
+  const Display = ({ counter }) => <div>{counter}</div>
+  const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
-  );
-};
-
+  )
+}
 
 export default App
