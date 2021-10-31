41b4a7c1155b59e219d9e8798317ec036ed4b299
ce13ste <celeste.lhr@live.com>
2021-10-31 23:27:00 +0800

    1.6: unicafe step1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const Display = ({ counter }) => <div>{counter}</div>
-  const Button = ({ onClick, text }) => (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const Button = ({ handleClick, text }) => {
+    return <button onClick={handleClick}>{text}</button>;
+  };
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />           
+      <h1>Please provide your feedback:</h1>
+      <Button handleClick={handleGoodClick} text='Good' />
+      <Button handleClick={handleNeutralClick} text='Neutral' />
+      <Button handleClick={handleBadClick} text='Bad' />
+
+
+      <h1>Statistics:</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   )
+
 }
 
 export default App
+
+
+
+
