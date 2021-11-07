1bdd69aae9ba8ebb25c64548d55647b007b763de
ce13ste <celeste.lhr@live.com>
2021-11-07 23:24:33 +0800

    1.7 unicafe step2

--- src/App.js
+++ src/App.js
@@ -5,26 +5,42 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [score, setScore] = useState(0);
 
   const Button = ({ handleClick, text }) => {
     return <button onClick={handleClick}>{text}</button>;
   };
-  const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setNeutral(neutral + 1);
-  const handleBadClick = () => setBad(bad + 1);
+
+  const clickGood = () => {
+        setGood(good + 1);
+        setTotal(total + 1);
+        setScore(score + 1);
+  };
+  const clickNeutral = () => {
+    setNeutral(neutral + 1);
+    setTotal(total + 1);
+    setScore(score);
+  };
+  const clickBad = () => {
+    setBad(bad + 1);
+    setTotal(total + 1);
+    setScore(score - 1);
+  };
 
   return (
     <div>
       <h1>Please provide your feedback:</h1>
-      <Button handleClick={handleGoodClick} text='Good' />
-      <Button handleClick={handleNeutralClick} text='Neutral' />
-      <Button handleClick={handleBadClick} text='Bad' />
-
+      <Button handleClick={clickGood} text='Good' />
+      <Button handleClick={clickNeutral} text='Neutral' />
+      <Button handleClick={clickBad} text='Bad' />
 
       <h1>Statistics:</h1>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>Average {score / total}</p>
+      <p>Positive {(good/(total))*100} %</p>
     </div>
   )
 
