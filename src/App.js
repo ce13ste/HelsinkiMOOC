fc45035531dc4315ae41710c013fcb73a399fe6a
ce13ste <celeste.lhr@live.com>
2022-03-07 18:11:30 +0800

    2.7: The Phonebook Step2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import Name from "./components/Name";
 
 const App = () => {
 	console.log("app");
-	const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+	const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 },{ name: "Arto Bru", id: 2 }]);
 	const [newName, setNewName] = useState("");
 
-	const addName = (event) => {
-		event.preventDefault();
+	const addName = () => {
 		const nameObject = {
 			name: newName,
 			id: persons.length + 1,
@@ -19,12 +18,22 @@ const App = () => {
 
 	const updateName = (event) => {
 		setNewName(event.target.value);
-	};
+  };
+  
+  const checkExists = (event) => {
+    event.preventDefault();
+
+    const currentList = persons.map((person) => person.name);
+
+    currentList.includes(newName)
+      ? alert(`${newName} is already added to the phontbook`)
+      : addName();
+  };
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form onSubmit={addName}>
+			<form onSubmit={checkExists}>
 				<input value={newName} onChange={updateName} />
 				<button type="submit">add</button>
 			</form>
