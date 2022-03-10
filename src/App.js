10373f004c141963fc11bce4ca529f2bc1c109c7
ce13ste <celeste.lhr@live.com>
2022-03-10 23:07:13 +0800

    2.10: The Phonebook Step5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import Name from "./components/Name";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
 	console.log("app");
@@ -11,71 +14,22 @@ const App = () => {
     { name: "Fish Ng", number: "652-124518", id: 5 },
 
   ]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
+
   const [filter, setFilter] = useState("");
-
-	const addName = () => {
-		const nameObject = {
-      name: newName,
-      number: newNumber,
-			id: persons.length + 1,
-		};
-
-		setPersons(persons.concat(nameObject));
-    setNewName("")
-    setNewNumber("");
-	};
-
-	const updateName = (event) => {
-		setNewName(event.target.value);
-  };
-
-  const updateNumber = (event) => {
-		setNewNumber(event.target.value);
-  };
-
-  const updateFilter = (event) => {
-		setFilter(event.target.value);
-  };
   
   const filteredNames = persons.filter(
     (person) => person.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1
   );
 
-  const checkExists = (event) => {
-    event.preventDefault();
-
-    const currentList = persons.map((person) => person.name);
-
-    currentList.includes(newName)
-      ? alert(`${newName} is already added to the phontbook`)
-      : addName();
-  };
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
-      <div>
-          filter shown with: <input value={filter} onChange={updateFilter} />
-        </div>
+      <Filter filter={filter} setFilter={setFilter} />
       <h2>add a new</h2>
-			<form onSubmit={checkExists}>
-				<div>
-          name: <input value={newName} onChange={updateName} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={updateNumber} />
-        </div>
-				<button type="submit">add</button>
-			</form>
+			<PersonForm persons={persons} setPersons={setPersons} />
 			<h2>Numbers</h2>
-			<ul>
-				{filteredNames.map(person => (
-					<Name key={person.id} person={person} />
-          
-				))}
-			</ul>
+			<Persons filteredNames={filteredNames} />
 		</div>
 	);
 };
